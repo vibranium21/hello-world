a836965a03c89a4de694b6d0de1f33600fce1bf6
vibranium21 <84234931+vibranium21@users.noreply.github.com>
2021-06-03 14:29:26 -0400

    Update helloworld.rs

--- helloworld.rs
+++ helloworld.rs
@@ -1,9 +1,4 @@
-// This is a comment, and is ignored by the compiler
-// You can test this code by clicking the "Run" button over there ->
-// or if you prefer to use your keyboard, you can use the "Ctrl + Enter" shortcut
 
-// This code is editable, feel free to hack it!
-// You can always return to the original code by clicking the "Reset" button ->
 
 // This is the main function
 fn main() {
