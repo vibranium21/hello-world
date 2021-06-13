a5513cf7d8d582841936f034e5018dd1b757a18e
Virinas-code <61283052+Virinas-code@users.noreply.github.com>
2021-06-13 16:19:33 +0200

    Update calculator.py

--- hello_projects/calculator.py
+++ hello_projects/calculator.py
@@ -23,23 +23,24 @@ def divide(val1, val2):
     return val1 / val2
 
 
-print("Select operation")
-print("1. Add")
-print("2. Subtract")
-print("3. Multiply")
-print("4. Division")
-while True:
-    choice = input(" Enter choice (1/2/3/4) : ")
-    if choice in ('1', '2', '3', '4'):
-        num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number: "))
-        if choice == '1':
-            print(num1, "+", num2, "=", add(num1, num2))
-        elif choice == '2':
-            print(num1, "-", num2, "=", subtract(num1, num2))
-        elif choice == '3':
-            print(num1, "*", num2, "=", multiply(num1, num2))
-        elif choice == '4':
-            print(num1, "/", num2, "=", divide(num1, num2))
-        break
-    print("Invalid Input")
+if __name__ == "__main__":
+    print("Select operation")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Division")
+    while True:
+        choice = input(" Enter choice (1/2/3/4) : ")
+        if choice in ('1', '2', '3', '4'):
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+            if choice == '1':
+                print(num1, "+", num2, "=", add(num1, num2))
+            elif choice == '2':
+                print(num1, "-", num2, "=", subtract(num1, num2))
+            elif choice == '3':
+                print(num1, "*", num2, "=", multiply(num1, num2))
+            elif choice == '4':
+                print(num1, "/", num2, "=", divide(num1, num2))
+            break
+        print("Invalid Input")
